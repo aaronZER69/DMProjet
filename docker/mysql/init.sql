ddcab3cd74cb94a408d88e6b67c297ba3ee1eaab
aaronZER69 <aaron.zerrouk96@gmail.com>
2025-04-30 16:49:15 +0200

    commit

--- docker/mysql/init.sql
+++ docker/mysql/init.sql
@@ -1,2 +1,44 @@
 CREATE DATABASE IF NOT EXISTS TDB;
-USE TDB
+USE TDB;
+
+CREATE TABLe clients(
+    id_client int primary key auto_increment,
+    nom varchar(100),
+    email varchar(100);
+    date_inscription DATE
+);
+
+create table performance(
+    id_perf int primary key auto_increment,
+    nv_clients int,
+    nb_ventes int,
+    objectif decimal(10,2),
+    taux_atteint decimal(10,2),
+    CA decimal (10,2)
+);
+
+create table objectif(
+    id_obj int primary key auto_increment,
+    obj_vente decimal(10,2),
+
+)
+
+create table produit(
+    id_prod int primary key auto_increment,
+    prix decimal(10,2),
+    nom varchar(200)
+);
+
+
+
+create table ventes(
+    id_vente int primary key auto_increment,
+    quantite int,
+    date date,
+    id_produit int,
+    id_client int,
+    foreign key (id_client) references clients(id_client),
+    foreign key (id_produit) references produit(id_prod),
+
+);
+
--- src/config/database.php
+++ src/config/database.php
@@ -1,9 +1,25 @@
 <?php
+class Database{
+    private static $pdo = null;
 
-return [
-    'host' => 'localhost',
-    'user' => 'root',
-    'password' => '',
-    'database' => 'TDB',
-    'charset' => 'utf8',
-];
+    public static function connect(){
+        if(self::$pdo == null){
+            $host = "mysql";
+            $dbname = "tdb";
+            $user = "root";
+            $pass = "root";
+            $charset = "utf8";
+
+            try {
+                self::$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+                self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            } catch (PDOException $e) {
+                die('Erreur connexion BDD : ' . $e->getMessage());
+            }
+        }
+        return self::$pdo;
+    }
+
+
+    }
+
